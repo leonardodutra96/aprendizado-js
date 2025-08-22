7794b1ad595de4c00eac45d93e183a3e8992bde9
Leonardo Dutra <65054050+leozinfln@users.noreply.github.com>
2025-08-22 19:31:21 -0300

    Versão 001 - Formulário básico com validação de email e senha

---
+++ script/javascript.js
@@ -0,0 +1,40 @@
+function confirmar() {
+    //VARIAVEIS
+    let nome, sobrenome, telefone, email, senha, sucesso, semsucesso, campovazio, senhacurta
+    // VALORES DAS VARIAVEIS
+    nome = document.getElementById("nome").value.trim()
+    sobrenome = document.getElementById("sobrenome").value.trim()
+    telefone = document.getElementById("telefone").value.trim()
+    email = document.getElementById("email").value.trim()
+    cemail = document.getElementById("cemail").value.trim()
+    senha = document.getElementById("password").value.trim()
+    csenha = document.getElementById("cpassword").value.trim()
+    sucesso = ("Cadastro realizado com sucesso!")
+    semsucesso = ("Não foi possivel realizar o cadastro")
+    campovazio = ("Precisa preencher todos os campos")
+    senhacurta = ("Senha curta demais")
+
+    // CONDIÇÕES
+    if (!nome || !sobrenome || !telefone || !email || !cemail || !senha || !csenha) {
+        alert(campovazio)
+        return
+    }
+    if (senha.length <8|| csenha.length < 8) {
+        alert(senhacurta)
+        return
+    }
+
+    if (email === cemail && senha === csenha) {
+        alert(sucesso)
+        document.getElementById("cadastrook").innerHTML = sucesso
+        return
+    }
+
+    else {
+        alert(semsucesso)
+        document.getElementById("cadastrook").innerHTML = semsucesso
+
+        return
+    }
+
+}
